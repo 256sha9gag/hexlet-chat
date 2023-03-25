a906e6acdce19a80db6a661f087fea4e5b0aef0e
gag <256sha9gag@gmail.com>
2023-03-25 16:18:26 +0300

    refactor routes

--- frontend/src/components/ChatPrivateRoute.jsx
+++ frontend/src/components/ChatPrivateRoute.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { useLocation, Navigate } from 'react-router-dom';
+
 import useAuth from '../hooks/index.jsx';
+import routes from '../routes';
 
 const PrivateRoute = ({ children }) => {
   const auth = useAuth();
   const location = useLocation();
 
   return (
-    auth.signedIn ? children : <Navigate to="/login" state={{ from: location }} />
+    auth.signedIn ? children : <Navigate to={routes.singInPage()} state={{ from: location }} />
   );
 };
 
