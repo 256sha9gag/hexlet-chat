64fbeb94010b153b5e7b2168b0512d0f1f686989
gag <256sha9gag@gmail.com>
2023-03-25 15:44:10 +0300

    refactor: navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { Container, Navbar, Button } from 'react-bootstrap';
+import {
+  Container, Navbar, Button,
+} from 'react-bootstrap';
 import useAuth from '../hooks';
+import routes from '../routes';
 
 const AuthButton = () => {
-  const auth = useAuth();
   const location = useLocation();
+  const auth = useAuth();
 
   return (
     auth.signedIn
       ? <Button onClick={auth.signOut}>Log out</Button>
-      : <Button as={Link} to="/login" state={{ from: location }}>Sign in</Button>
+      : <Button as={Link} to={routes.singInPage()} state={{ from: location }}>Sign in</Button>
   );
 };
 
 const NavBar = () => (
   <Navbar expand="lg" variant="dark" bg="dark">
     <Container>
-      <Navbar.Brand as={Link} to="/">My Chat</Navbar.Brand>
+      <Navbar.Brand as={Link} to={routes.mainPage()}>My Chat</Navbar.Brand>
       <AuthButton />
     </Container>
   </Navbar>
