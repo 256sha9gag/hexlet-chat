353ea02e931c2a7d93733f009cf29696be531023
gag <256sha9gag@gmail.com>
2023-03-25 16:16:10 +0300

    refactor: position notFoundPage

--- frontend/src/pages/NotFoundPage.jsx
+++ frontend/src/pages/NotFoundPage.jsx
@@ -1,23 +1,31 @@
 import Card from 'react-bootstrap/Card';
+import { Container, Row, Col } from 'react-bootstrap';
 
-const NotFound = () => (
-  <div className="d-flex flex-column h-100">
-    <Card className="mt-5 mx-auto">
-      <Card.Body>
-        <Card.Title className="text-center">Error 404</Card.Title>
-        <Card.Text className="text-center">
-          Something seems to have gone wrong!
-          The page you requested does not exist.
-          Perhaps it is out of date, has been deleted,
-          or you entered the wrong URL in the address bar.
-        </Card.Text>
-      </Card.Body>
-      <Card.Footer className="text-center">
-        But you can go to
-        <a href="/"> the home page</a>
-      </Card.Footer>
-    </Card>
-  </div>
+import routes from '../routes';
+
+const NotFoundPage = () => (
+  <Container className="h-100">
+    <Row className="justify-content-center align-content-center h-100">
+      <Col className="col-6 col-md-4 col-xxl-4">
+        <Card className="mb-5 text-center">
+          <Card.Body>
+            <Card.Title>Error 404</Card.Title>
+            <Card.Text>
+              Something seems to have gone wrong!
+              The page you requested does not exist.
+              Perhaps it is out of date, has been deleted,
+              or you entered the wrong URL in the address bar.
+            </Card.Text>
+          </Card.Body>
+          <Card.Footer>
+            But you can go to
+            {' '}
+            <a href={routes.mainPage()}>the home page</a>
+          </Card.Footer>
+        </Card>
+      </Col>
+    </Row>
+  </Container>
 );
 
-export default NotFound;
+export default NotFoundPage;
