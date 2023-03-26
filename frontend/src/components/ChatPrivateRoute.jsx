01903912dd7184683dc8e971be99d11877aa1b0e
gag <256sha9gag@gmail.com>
2023-03-26 21:07:04 +0300

    refactor: ChatPriveteRoute

--- frontend/src/components/ChatPrivateRoute.jsx
+++ frontend/src/components/ChatPrivateRoute.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useLocation, Navigate } from 'react-router-dom';
 
-import useAuth from '../hooks/index.jsx';
+import useAuth from '../hooks/authContext';
 import routes from '../routes';
 
 const PrivateRoute = ({ children }) => {
