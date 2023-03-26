3db62d1dee4a7a8480025604e6de6e7e5df3fe11
gag <256sha9gag@gmail.com>
2023-03-26 21:08:36 +0300

    refactor: store

--- frontend/src/store/index.jsx
+++ frontend/src/store/index.jsx
@@ -1,9 +1,12 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { configureStore } from '@reduxjs/toolkit';
+
 import channelsReducer from './slice/channelSlice';
+import currentChannelIdReducer from './slice/currentChannelIdSlice';
+import messagesReducer from './slice/messagesSlice';
 
 const store = configureStore({
-  reducer: { channelsReducer },
+  reducer: { channelsReducer, currentChannelIdReducer, messagesReducer },
 });
 
 export default store;
