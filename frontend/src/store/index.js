2b34b81b9dad9d54e6d49531c945dad4643389af
gag <256sha9gag@gmail.com>
2023-03-30 00:50:47 +0300

    refactor: store

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -5,10 +5,11 @@ import channelsReducer from './slice/channelSlice';
 import currentChannelIdReducer from './slice/currentChannelIdSlice';
 import messagesReducer from './slice/messagesSlice';
 import messagesCountReducer from './slice/messagesCountSlice';
+import modalReducer from './slice/modalSlice';
 
 const store = configureStore({
   reducer: {
-    channelsReducer, currentChannelIdReducer, messagesReducer, messagesCountReducer,
+    channelsReducer, currentChannelIdReducer, messagesReducer, messagesCountReducer, modalReducer,
   },
 });
 
