dca654687c70ace450517a4333ea4eb190c19126
gag <256sha9gag@gmail.com>
2023-03-30 00:50:17 +0300

    refactor

--- frontend/src/hooks/socketContext.jsx
+++ frontend/src/hooks/socketContext.jsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 
-import SocketContext from '../context/SocketContext';
+import { SocketContext } from '../context/SocketProvider';
 
 const useSocket = () => useContext(SocketContext);
 
